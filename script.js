faa1326a2c78401617da3becff49f1632d493eb4
Nurzada <nurzadabakytkyzy8@gmail.com>
2024-05-03 12:06:43 -0700

    P1

---
+++ script.js
@@ -0,0 +1,48 @@
+
+const fs =require("fs");
+const inputText = fs.readFileSync("input.txt", "utf8");
+const words = inputText.split(/\s+/);
+const wordMap = new Map();
+let code = 0;
+words.forEach((word) => {
+  if (!wordMap.has(word)) {
+    wordMap.set(word, code++);
+  }
+});
+
+const compressedText = words.map((word) => wordMap.get(word)).join(" ");
+fs.writeFileSync("output.sc", compressedText);
+const compressedDate = fs.readFileSync("output.sc", "utf8");
+const codes = compressedDate.split(" ");
+const decompressedText = codes
+  .map((code) => {
+    return [...wordMap.keys()][code];
+  })
+  .join(" ");
+
+fs.writeFileSync("readable.txt", decompressedText);
+//Read both input and decompressed files
+const inputBuffer = fs.readFileSync("input.txt");
+const decompressedBuffer = fs.readFileSync("readable.txt");
+
+// Check if both files have the same byte length
+const isEqualLength = inputBuffer.length === decompressedBuffer.length;
+
+// Compare each byte of both files
+let isIdentical = true;
+if (isEqualLength) {
+  for (let i = 0; i < inputBuffer.length; i++) {
+    if (inputBuffer[i] !== decompressedBuffer[i]) {
+      isIdentical = false;
+      break;
+    }
+  }
+} else {
+  isIdentical = false;
+}
+
+if (isIdentical) {
+  console.log("Files are identical.");
+} else {
+  console.log("Files are not identical.");
+}
